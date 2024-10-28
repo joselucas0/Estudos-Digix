9498c57831c053abdfb8dd38c4456d276d6acb07
joselucas0 <jose.l@ufms.br>
2024-10-28 13:27:03 -0400

    commit

---
+++ Aula2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula_2{
+    class Program
+    {
+            static void Main(string[] args){
+        int numero1 = 5, numero2 = 10, resultado, mult, div;
+        resultado = numero1 + numero2;
+        Console.WriteLine("A soma de {0} e {1} é {2}", numero1, numero2, resultado);
+        mult = numero1 * numero2;
+        div = numero1 / numero2;
+        Console.WriteLine("A multiplicação de {0} e {1} é {2}", numero1, numero2, mult);
+        Console.WriteLine("A divisão de {0} e {1} é {2}", numero1, numero2, div);
+        double raiz = Math.Sqrt(numero1);
+        Console.WriteLine("A raiz quadrada de {0} é {1}", numero1, raiz);
+
+        //incrementando um número
+        numero1++;
+        Console.WriteLine("Incrementando um número: {0}", numero1);
+        //operadores de comparação
+        if(numero1 > numero2){
+            Console.WriteLine("{0} é maior que {1}", numero1, numero2);
+            }else if(numero1 < numero2){
+            Console.WriteLine("{0} é menor que {1}", numero1, numero2);
+            } else{
+            Console.WriteLine("{0} é igual a {1}", numero1, numero2);
+            }
+        }
+    }
+    }
---
+++ Aula3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+
+namespace Program
+{
+
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public int Matricula { get; set; }
+        public Curso Curso { get; set; }
+
+        public Aluno(string nome, int matricula, Curso curso)
+        {
+            Nome = nome;
+            Matricula = matricula;
+            Curso = curso;
+        }
+        //exibir dados do aluno
+        public void ExibirDados()
+        {
+            Console.WriteLine($"Nome: {Nome}, Matrícula: {Matricula}, Curso: {Curso.nomeCurso}");
+            Curso.ExibirDisciplinas();
+        }
+
+
+    }
+    class Curso
+    {
+        public string nomeCurso { get; set; }
+        public int codigoCurso { get; set; }
+        public List<Disciplina> Disciplinas { get; set; }
+
+
+        public Curso(string nomeCurso, int codigoCurso)
+        {
+            this.nomeCurso = nomeCurso;
+            this.codigoCurso = codigoCurso;
+            Disciplinas = new List<Disciplina>();
+        }
+
+
+        public void AdicionarDisciplina(Disciplina disciplina)
+        {
+            Disciplinas.Add(disciplina);
+        }
+
+
+        public void RemoverDisciplina(Disciplina disciplina)
+        {
+            Disciplinas.Remove(disciplina);
+        }
+        }
+    
+        public void ExibirDisciplinas()
+        {
+            Console.WriteLine("Disciplinas do curso: {nomeCurso}");
+            foreach (Disciplina disciplina in Disciplinas)
+            {
+                Console.WriteLine($"Nome: {disciplina.Nome}, Código: {disciplina.Codigo}");
+            }
+        }
+
+    }
+    class Disciplina
+{
+    public string nomeDisciplina { get; set; }
+    public int codigoDisciplina { get; set; }
+
+
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+
+}
+
+    }
