d7bf99248332e4f161bcbcebbdb7fc8202f7d3e6
joselucas0 <jose.l@ufms.br>
2024-11-25 13:35:50 -0400

    feat: adicionando dependencias

--- Aula2/obj/Debug/net8.0/Aula2.AssemblyInfo.cs
+++ Aula2/obj/Debug/net8.0/Aula2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8636ff782946b4e1351f6c480a6d5e523663113")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7d154ff3e38829a4c272690bf2305ae2ac7c900")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
